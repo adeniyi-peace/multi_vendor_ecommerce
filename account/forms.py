d9b83cce08856a3140687b285ca3498cbcee14fc
Adeniyi Peace <adeniyipeaceo1@gmail.com>
2024-11-07 00:23:43 +0100

    set email to be used for logging in instead of username

--- account/forms.py
+++ account/forms.py
@@ -1,13 +1,16 @@
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
 from django.forms import ModelForm
 
+from .models import CustomUser
+
 class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
-        model = User
+        model = CustomUser
         fields = ["username", "email", "password1", "password2"]
 
 class UserLoginForm(AuthenticationForm):
--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/login/", views.LoginView.as_view(), name="login"),
-    path("/sign-up/", views.RegisterUserView.as_view(), name="sign_up"),
-    path("/logout/", views.LogoutView.as_view(), name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("sign-up/", views.RegisterUserView.as_view(), name="sign_up"),
+    path("logout/", views.LogoutView.as_view(), name="login"),
 ]
